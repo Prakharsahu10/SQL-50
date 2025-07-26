79187136e56e03c5ff382434d9eb08d44fcbc49b
Prakhar Sahu <prakhars762@gmail.com>
2025-07-26 12:56:43 +0530

    advanced select and joins

---
+++ ADVANCED SELECT AND JOINS/program_4.sql
@@ -0,0 +1,52 @@
+/* Consecutive Numbers */
+/*
+
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column starting from 1.
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+*/
+
+SELECT DISTINCT log1.num AS ConsecutiveNums
+FROM Logs AS log1, Logs AS log2, Logs AS log3
+WHERE
+log1.id + 1 = log2.id AND
+log2.id + 1 = log3.id AND
+log1.num = log2.num AND
+log2.num = log3.num
